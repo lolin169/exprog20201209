f7327863724c9947bb8044bb9af2b8fbe78cf5bc
alumno <alumno@192.168.56.1>
2020-12-09 16:49:39 +0100

    Ejercicio 2 resuelto

---
+++ src/fp/daw/examen1ev/Ejercicio2.java
@@ -0,0 +1,32 @@
+package fp.daw.examen1ev;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+	static Scanner teclado = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		int ano = 2020;
+		int ano1 = 100;
+		int ano2 = 400;
+		int ano3= 4;
+		System.out.printf("El año %d %s\n", ano, esBisiesto(ano) ? "es bisiesto" : "No es bisiesto");
+		System.out.printf("El año %d %s\n", ano1, esBisiesto(ano1) ? "es bisiesto" : "No es bisiesto");
+		System.out.printf("El año %d %s\n", ano2, esBisiesto(ano2) ? "es bisiesto" : "No es bisiesto");
+		System.out.printf("El año %d %s\n", ano3, esBisiesto(ano3) ? "es bisiesto" : "No es bisiesto");
+       
+	}
+
+	static boolean esBisiesto(int n) {
+		boolean bisiesto = false;
+		if ((n % 100) == 0 ^ ((n % 400 == 0)&& (n % 4 == 0))) {
+			bisiesto = false;
+			return bisiesto;
+		}
+		bisiesto = true;
+
+		return bisiesto;
+	}
+
+}
